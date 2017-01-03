525d815e2ed2522c9755409d47d5f72c570bcceb
Yuriy Chaban <Yuriy Chaban>
2017-01-03 17:11:07 +0200

    add html on main page & stylng header

---
+++ js/googleMapCustomize.js
@@ -0,0 +1,170 @@
+	try {
+
+		var point = {
+			lat: 42.044824,
+			lng: -88.0460074
+		};
+		var markerSize = {
+			x: 76,
+			y: 85
+		};
+
+
+		google.maps.Marker.prototype.setLabel = function(label) {
+			this.label = new MarkerLabel({
+				map: this.map,
+				marker: this,
+				text: label
+			});
+			this.label.bindTo('position', this, 'position');
+		};
+
+		var MarkerLabel = function(options) {
+			this.setValues(options);
+			this.span = document.createElement('span');
+			this.span.className = 'map-marker-label';
+		};
+
+		MarkerLabel.prototype = $.extend(new google.maps.OverlayView(), {
+			onAdd: function() {
+				this.getPanes().overlayImage.appendChild(this.span);
+				var self = this;
+				this.listeners = [
+				google.maps.event.addListener(this, 'position_changed', function() {
+					self.draw();
+				})
+				];
+			},
+			draw: function() {
+				var text = String(this.get('text'));
+				var position = this.getProjection().fromLatLngToDivPixel(this.get('position'));
+				this.span.innerHTML = text;
+				this.span.style.left = (position.x - (markerSize.x / 2)) - (text.length * 3) + 330 + 'px';
+				this.span.style.top = (position.y - markerSize.y - 5) + 'px';
+			}
+		});
+
+		function initialize() {    
+			var myLatLng = new google.maps.LatLng(point.lat, point.lng);
+			var gmap = new google.maps.Map(document.getElementById('mapWrapper'), {
+				zoom: 17,
+				center: myLatLng,
+				mapTypeId: google.maps.MapTypeId.ROADMAP,
+				styles: [
+				{
+					"featureType": "all",
+					"elementType": "labels.text.stroke",
+					"stylers": [
+					{
+						"visibility": "off"
+					}
+					]
+				},
+				{
+					"featureType": "administrative.neighborhood",
+					"elementType": "labels.text.fill",
+					"stylers": [
+					{
+						"color": "#333333"
+					}
+					]
+				},
+				{
+					"featureType": "poi.business",
+					"elementType": "labels.text",
+					"stylers": [
+					{
+						"visibility": "off"
+					}
+					]
+				},
+				{
+					"featureType": "poi.business",
+					"elementType": "labels.icon",
+					"stylers": [
+					{
+						"visibility": "off"
+					}
+					]
+				},
+				{
+					"featureType": "poi.place_of_worship",
+					"elementType": "labels.text",
+					"stylers": [
+					{
+						"visibility": "off"
+					}
+					]
+				},
+				{
+					"featureType": "poi.place_of_worship",
+					"elementType": "labels.icon",
+					"stylers": [
+					{
+						"visibility": "off"
+					}
+					]
+				},
+				{
+					"featureType": "road",
+					"elementType": "geometry",
+					"stylers": [
+					{
+						"visibility": "simplified"
+					}
+					]
+				},
+				{
+					"featureType": "road.local",
+					"elementType": "labels.text",
+					"stylers": [
+					{
+						"weight": 0.5
+					},
+					{
+						"color": "#333333"
+					}
+					]
+				},
+				{
+					"featureType": "transit.station",
+					"elementType": "labels.icon",
+					"stylers": [
+					{
+						"gamma": 1
+					},
+					{
+						"saturation": 50
+					}
+					]
+				},
+				{
+					"featureType": "water",
+					"elementType": "all",
+					"stylers": [
+					{
+						"visibility": "on"
+					},
+					{
+						"saturation": 50
+					},
+					{
+						"hue": "#50a5d1"
+					}
+					]
+				}
+				]
+			});
+			var myMarker = new google.maps.Marker({
+				map: gmap,
+				icon: "images/map-marker.png",
+				position: myLatLng,
+				label: 'International Training Academy',
+				draggable: true
+			});
+		}
+
+		initialize();  
+	} catch (e) {
+		alert(e);
+	}
---
+++ js/main.js
@@ -0,0 +1,65 @@
+// FOR DROPDOWN ACTIVE
+$(document).ready(function($) {
+	$('.ui.dropdown')
+	.dropdown();
+}); 
+// FIND DROPDOVN AND ADD CLASS ANGLE DOWN IN SELECT BLOCK
+$(document).ready(function($) {
+	$(".select-lenguage").find(".icon").addClass('angle down');
+});
+// MOBILE MENU
+$('#toggle').click(function() {
+	$(this).toggleClass('active');
+	$('#overlay').toggleClass('open');
+	$('#toggle').parents('.inside-page-header').css('padding: 0;')
+});
+
+// MAIN PAGE SLIDER
+$('.main-slider').slick({
+	autoplay: false,
+	autoplaySpeed: 3000,
+	infinite: true,
+	speed: 1000,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	centerPadding: '0',
+	arrows: true,
+	nextArrow: '<div class="next-btn"><i class="fa fa-angle-right" aria-hidden="true"></i></div>',
+	prevArrow: '<div class="prev-btn"><i class="fa fa-angle-left" aria-hidden="true"></i></div>'
+});
+
+// GALLERY SLIDER ON MAIN PAGE
+$('.gallery-slider').slick({
+	autoplay: false,
+	autoplaySpeed: 3000,
+	infinite: true,
+	speed: 1000,
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	centerPadding: '20px',
+	variableWidth: true,
+	responsive: [
+	{
+		breakpoint: 768,
+		settings: {
+			arrows: false,
+			centerMode: true,
+			centerPadding: '20px',
+			slidesToShow: 2
+		}
+	},
+	{
+		breakpoint: 480,
+		settings: {
+			arrows: true,
+			centerMode: true,
+			centerPadding: '20px',
+			slidesToShow: 1
+		}
+	}
+	],
+	arrows: true,
+	nextArrow: '<div class="next-btn"><i class="fa fa-angle-right" aria-hidden="true"></i></div>',
+	prevArrow: '<div class="prev-btn"><i class="fa fa-angle-left" aria-hidden="true"></i></div>'
+
+});
