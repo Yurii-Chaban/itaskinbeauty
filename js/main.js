fcc572f33156bf0c93213067fed2abb0a09bb42b
Yuriy Chaban <Yuriy Chaban>
2017-01-24 12:44:42 +0200

    fix initial slider by list of id

--- js/main.js
+++ js/main.js
@@ -291,13 +291,17 @@ $(document).ready(function() {
 
 });
 
+
+
+function openPopup(id)
+{
+	$(id).addClass('is-visible');
+	console.log("good");
+}
+
 jQuery(document).ready(function($){
-	//open popup
-	$('.cd-popup-trigger').on('click', function(event){
-		event.preventDefault();
-		$('.cd-popup').addClass('is-visible');
-	});
-	
+
+
 	//close popup
 	$('.cd-popup').on('click', function(event){
 		if( $(event.target).is('.cd-popup-close') || $(event.target).is('.cd-popup') ) {
@@ -312,6 +316,29 @@ jQuery(document).ready(function($){
 		}
 	});
 });
+
+
+// jQuery(document).ready(function($){
+// 	//open popup
+// 	$('.cd-popup-trigger').on('click', function(event){
+// 		event.preventDefault();
+// 		$('.cd-popup').addClass('is-visible');
+// 	});
+
+// 	//close popup
+// 	$('.cd-popup').on('click', function(event){
+// 		if( $(event.target).is('.cd-popup-close') || $(event.target).is('.cd-popup') ) {
+// 			event.preventDefault();
+// 			$(this).removeClass('is-visible');
+// 		}
+// 	});
+// 	//close popup when clicking the esc keyboard button
+// 	$(document).keyup(function(event){
+// 		if(event.which=='27'){
+// 			$('.cd-popup').removeClass('is-visible');
+// 		}
+// 	});
+// });
 
 $(document).ready(function() {
 	$(".sub-menu-mobile > a")['on']("click", function(){
